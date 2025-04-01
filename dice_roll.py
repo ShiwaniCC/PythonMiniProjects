58553b463a74aafc6a86d62ffd5709a8127ac478
Shiwani <shiwani.soni@codeclouds.co.in>
2025-04-01 12:44:09 +0530

    Dice Roll Done

---
+++ dice_roll.py
@@ -0,0 +1,9 @@
+# if else.. yes and no
+import random
+c = input ("Do you want to Roll? ")
+if(c == "Y"):
+    print ("hello world")
+    num = random.randint(1,6)
+    print (num)
+else: 
+    print ("Happy coding")
