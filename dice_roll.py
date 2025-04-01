190cc37d02c20dcd2f4d2bdd0562551561b87d61
Shiwani <shiwani.soni@codeclouds.co.in>
2025-04-01 16:04:41 +0530

    Dice Roll done

--- dice_roll.py
+++ dice_roll.py
@@ -1,9 +1,14 @@
-# if else.. yes and no
 import random
-c = input ("Do you want to Roll? ")
-if(c == "Y"):
-    print ("hello world")
-    num = random.randint(1,6)
-    print (num)
-else: 
-    print ("Happy coding")
+
+while True:
+    c = input("Do you want to Roll(Y/N)? ").upper()
+
+    if c == "Y":
+        num1 = random.randint(1, 6)
+        num2 = random.randint(1, 6)
+        print(f'({num1} , {num2})')
+    elif c == "N":
+        print("Thanks for playing!!")
+        break
+    else:
+        print("Invalid Input! Please enter 'Y' or 'N'.")
