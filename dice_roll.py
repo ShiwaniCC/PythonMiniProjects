4342ad4b592a12bd4cb2c10cd644eb2433c718b3
Shiwani <shiwani.soni@codeclouds.co.in>
2025-04-02 18:16:40 +0530

    count added

--- dice_roll.py
+++ dice_roll.py
@@ -1,8 +1,10 @@
 import random
 
+count = 0
+i = 1
 while True:
+    count = count + 1
     c = input("Do you want to Roll(Y/N)? ").upper()
-
     if c == "Y":
         num1 = random.randint(1, 6)
         num2 = random.randint(1, 6)
@@ -12,3 +14,5 @@ while True:
         break
     else:
         print("Invalid Input! Please enter 'Y' or 'N'.")
+
+    print(f"Number of times rolled so far: {count}")
